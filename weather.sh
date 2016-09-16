76180a45adc2f04b4c3916c45878b91910f3c3a3
Krishna Bhattarai <krishnabhattarai@i21b-s2.ufhpc>
2016-09-16 11:49:37 -0400

    change 1 yesterday's maximum temp

--- weather.sh
+++ weather.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+year=`date -d yesterday +%Y`
+month=`date -d yesterday +%M`
+day=`date -d yesterday +%D`
 
-curl "https://www.wunderground.com/history/airport/GNV/2016/09/06/DailyHistory.heml?&format=1" > gnv.txt
+curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day/DailyHistory.heml?&format=1" > MaxTemp.txt
+
+
+
 
 maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
 
 echo The Max temp is $maxTemp
+
+
 
